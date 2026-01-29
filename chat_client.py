950c19d8f3ba0a4f27f1d965c475ed9a8e3e17d2
Lek <ligioner29@mail.ru>
2026-01-29 17:52:36 +0800

    Save messages history to file.

--- chat_client.py
+++ chat_client.py
@@ -1,23 +1,54 @@
 import asyncio
+import aiofiles
+
+from datetime import datetime
 
 
 async def main():
-    reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5000)
+    host = 'minechat.dvmn.org'
+    port = 5000
+    logfile = 'chat_logfile.txt'
 
-    try:
-        while True:
-            line = await reader.readline()
+    writer = None
 
-            message = line.decode().strip()
+    while True:
+        try:
+            reader, writer = await asyncio.open_connection(host, port)
 
-            if not message:
-                break
+            async with aiofiles.open(logfile, mode='a', encoding='utf-8') as f:
+                timestamp = datetime.now().strftime('%d.%m.%y %H:%M')
+                start_msg = f"[{timestamp}] Установлено соединение\n"
 
-            print(message)
-    finally:
-        writer.close()
-        await writer.wait_closed()
+                print(start_msg)
 
+                await f.write(start_msg)
+                await f.flush()
+
+            while True:
+                encoded_message = await reader.readline()
+
+                if not encoded_message:
+                    break
+
+                decoded_message = encoded_message.decode().strip()
+                timestamp = datetime.now().strftime('%d.%m.%y %H:%M')
+                formatted_log = f"[{timestamp}] {decoded_message}\n"
+
+                async with aiofiles.open(logfile, mode='a', encoding='utf-8') as f:
+                    await f.write(formatted_log)
+                    await f.flush()
+
+                print(formatted_log.strip())
+
+        except (ConnectionError, asyncio.TimeoutError, OSError):
+            await asyncio.sleep(5)
+        finally:
+            if writer:
+                writer.close()
+                await writer.wait_closed()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\nСкрипт остановлен пользователем.")
